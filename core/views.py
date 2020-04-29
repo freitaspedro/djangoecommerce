6290a653990e937984d514e5a926a312785ea095
pedro <pfreitas@id.uff.br>
2020-04-29 13:38:34 -0300

    building template with statics

--- core/views.py
+++ core/views.py
@@ -4,4 +4,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse('Hello World!')
+	texts = ['Lorem ipsum dolor sit amet', 'consectetur adipisicing elit', 'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua']
+	context = {
+		'title': 'django e-commerce',
+		'texts': texts
+	}
+	return render(request, 'index.html', context)
