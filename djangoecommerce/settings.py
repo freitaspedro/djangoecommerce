5f61a6551532234f70cf6a247d677414f63ca1be
pedro <pfreitas@id.uff.br>
2020-06-05 12:22:17 -0300

    adjusting to heroku deploy

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -25,7 +25,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = 'j086+gk(2g7!8zx67vk(k5=d2jgp$t9tf66+fpivn60ds_ovm9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
