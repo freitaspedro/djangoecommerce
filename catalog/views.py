b105a210510faecafcf4f66f47d78ae0eedc7d11
pedro <pfreitas@id.uff.br>
2020-08-09 15:34:03 -0300

    adding django watson to search

--- catalog/views.py
+++ catalog/views.py
@@ -3,15 +3,23 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
+from watson import search as watson
+
 from .models import Product, Category
 
 
 class ProductsListView(ListView):
 
-	model = Product
 	template_name = 'products.html'
 	context_object_name = 'products'
 	paginate_by = 3
+
+	def get_queryset(self):
+		queryset = Product.objects.all()
+		q = self.request.GET.get('q', '')
+		if q:
+			queryset = watson.filter(queryset, q)
+		return queryset
 
 
 products = ProductsListView.as_view()
